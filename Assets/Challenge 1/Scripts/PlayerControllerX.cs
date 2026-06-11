cd975b8fdcb43a7511a1ab3646f08b3b476873d9
Kinetic64 <ben.ward@simplygreat.education>
2026-06-11 13:58:25 +0100

    Plane neowm

---
+++ Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public float speed;
+    public float rotationSpeed;
+    public float verticalInput;
+    public InputAction moveAction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    moveAction.Enable();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // get the user's vertical input
+        verticalInput = Input.GetAxis("Vertical");
+
+        // move the plane forward at a constant rate
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+
+        // tilt the plane up/down based on up/down arrow keys
+        transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime * verticalInput);
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,26 +4,33 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    //Movement turning (Editable in inspector)
     public float speed = 5.0f;
     public float turnSpeed = 100f;
 
+    //Input System action (In inspector for key binding)
     public InputAction moveAction;
-    public Vector2 moveInput;
+
+    //Current input value (x = left/right y = forward/backward)
+    private Vector2 moveInput;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        //Enable the MoveAction so it starts reading input
         moveAction.Enable();
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Reads the 2D vector from the MoveAction (x = horizontal y = vertical)
         moveInput = moveAction.ReadValue<Vector2>();
 
-        //move the vehicle forward
+        //move the vehicle forward/backward
         transform.Translate(Vector3.forward * Time.deltaTime * speed * moveInput.y);
-        //move the vehicle sideways
+
+        //move the vehicle left/right
         transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * moveInput.x);
     }
 }
